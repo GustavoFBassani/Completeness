ee8c28b10337e0f44fe904875e45c5b397369bbb
Gustavo <gustavo_fbassani@hotmail.com>
2025-09-09 08:05:16 -0300

    Initial Commit

---
+++ Completeness/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  Completeness
+//
+//  Created by Gustavo Ferreira bassani on 09/09/25.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
