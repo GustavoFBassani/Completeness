38c4f42c71bef375c4f0796810378ac2e6b29fab
Gustavo <gustavo_fbassani@hotmail.com>
2025-09-10 15:15:40 -0300

    categories model completed

---
+++ Completeness/Models/Categories.swift
@@ -0,0 +1,18 @@
+//
+//  Untitled.swift
+//  Completeness
+//
+//  Created by Gustavo Ferreira bassani on 10/09/25.
+//
+
+
+enum Categories: String {
+    case health = "Health"
+    case work = "Work"
+    case hobby = "Hobby"
+    case sports = "Sports"
+    case studies = "Studies"
+    case home = "Home"
+    case feith = "Feith"
+    case other = "Other"
+}
