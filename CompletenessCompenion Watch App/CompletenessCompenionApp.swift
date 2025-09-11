2ec8396f6fb140aae4a3ed850d753dce4bb13f78
Gustavo <gustavo_fbassani@hotmail.com>
2025-09-11 15:33:04 -0300

    provisioning profiles updated

---
+++ CompletenessCompenion Watch App/CompletenessCompenionApp.swift
@@ -0,0 +1,17 @@
+//
+//  CompletenessCompenionApp.swift
+//  CompletenessCompenion Watch App
+//
+//  Created by Gustavo Ferreira bassani on 10/09/25.
+//
+
+import SwiftUI
+
+@main
+struct CompletenessCompenion_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
