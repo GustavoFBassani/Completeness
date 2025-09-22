c42002eb620eface22831669fdc5487fccb99b27
Pablo Garcia <pablorobertodev@gmail.com>
2025-09-22 16:15:06 -0300

    Toggle habit button done

--- Completeness/Components/ToggleHabitViewComponent.swift
+++ Completeness/Components/ToggleHabitViewComponent.swift
@@ -7,12 +7,46 @@
 
 import SwiftUI
 
-struct toggleHabitViewComponent: View {
+struct ToggleHabitViewComponent: View {
+    @State private var isDone = false
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack{
+            Button(action: {
+                withAnimation(.bouncy(duration: 1)) {
+                    isDone.toggle()
+                }
+            }) {
+                ZStack {
+                    Circle()
+                        .fill(isDone ? Color.white : Color.indigoCustom)
+                        .frame(width: 130, height: 130)
+                        .shadow(color: .black.opacity(0.25), radius: 34, x: 0, y: 4)
+                    
+                    VStack{
+                        Image(systemName: "book.fill")
+                            .resizable()
+                            .scaledToFit()
+                            .foregroundColor(isDone ? .indigoCustom : .white)
+                            .frame(width: 58, height: 58)
+                        
+                        if !isDone {
+                            Text("Feito!")
+                                .font(.system(size: 12.8, weight: .semibold))
+                                .foregroundColor(.white)
+                        }
+                    }
+                }
+            }
+            .buttonStyle(.plain)
+            
+            Text("Estudar")
+                .padding(.top, 6)
+                .font(.system(size: 12.8, weight: .semibold))
+                .foregroundColor(.labelPrimary)
+        }
     }
 }
 
 #Preview {
-    toggleHabitViewComponent()
+    ToggleHabitViewComponent()
 }
