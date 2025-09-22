96981499f0c048ce762c32fd245657a446fe9c12
Pablo Garcia <pablorobertodev@gmail.com>
2025-09-22 16:11:27 -0300

    Toggle habit component done

---
+++ Completeness/Components/ToggleHabitViewComponent.swift
@@ -0,0 +1,18 @@
+//
+//  habitViewComponent.swift
+//  Completeness
+//
+//  Created by Pablo Garcia-Dev on 22/09/25.
+//
+
+import SwiftUI
+
+struct toggleHabitViewComponent: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    toggleHabitViewComponent()
+}
