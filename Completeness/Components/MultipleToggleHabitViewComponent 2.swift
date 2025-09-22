70251d039259226b526986c41026cb0884919157
Pablo Garcia <pablorobertodev@gmail.com>
2025-09-22 17:25:47 -0300

    Multi toggle started

---
+++ Completeness/Components/MultipleToggleHabitViewComponent 2.swift
@@ -0,0 +1,68 @@
+//
+//  habitViewComponent.swift
+//  Completeness
+//
+//  Created by Pablo Garcia-Dev on 22/09/25.
+//
+
+import SwiftUI
+
+struct MultipleToggleHabitViewComponent: View {
+    @State private var progress = 0
+    let maxProgress = 5
+    
+    var body: some View {
+        VStack {
+            Button(action: {
+                withAnimation(.bouncy(duration: 0.6)) {
+                    if progress < maxProgress {
+                        progress += 1
+                    } else {
+                        progress = 0 // reinicia ou pode deixar fixo em 5
+                    }
+                }
+            }) {
+                ZStack {
+                    // Círculo de fundo
+                    Circle()
+                        .fill(progress == maxProgress ? Color.indigoCustom : Color.white)
+                        .frame(width: 130, height: 130)
+                        .shadow(color: .black.opacity(0.25), radius: 12, x: 0, y: 4)
+                    
+                    // Círculo de progresso
+                    Circle()
+                        .trim(from: 0, to: CGFloat(progress) / CGFloat(maxProgress))
+                        .stroke(progress == maxProgress ? Color.indigoCustom : Color.white,
+                                style: StrokeStyle(lineWidth: 6, lineCap: .round))
+                        .frame(width: 124, height: 124)
+                        .rotationEffect(.degrees(90)) // começa no topo
+                        .animation(.easeInOut, value: progress)
+                    
+                    VStack {
+                        Image(systemName: "book.fill")
+                            .resizable()
+                            .scaledToFit()
+                            .foregroundColor(progress == maxProgress ? .white : .indigoCustom)
+                            .frame(width: 58, height: 58)
+                        
+                        if progress < maxProgress {
+                            Text("\(progress)/\(maxProgress)")
+                                .font(.system(size: 12.8, weight: .semibold))
+                                .foregroundColor(.backgroundSecondary)
+                        }
+                    }
+                }
+            }
+            .buttonStyle(.plain)
+            
+            Text("Estudar")
+                .padding(.top, 6)
+                .font(.system(size: 12.8, weight: .semibold))
+                .foregroundColor(.labelPrimary)
+        }
+    }
+}
+
+#Preview {
+    MultipleToggleHabitViewComponent()
+}
