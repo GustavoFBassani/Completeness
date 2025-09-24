f602d056ebae72d3678117441512225bc6a9f714
Pablo Garcia <pablorobertodev@gmail.com>
2025-09-24 16:28:27 -0300

    translation started at localizable file

--- Completeness/App/TabBar.swift
+++ Completeness/App/TabBar.swift
@@ -12,39 +12,21 @@ struct TabBar: View {
     @Environment(\.modelContext) var context
     var body: some View {
         TabView{
-            Tab("Habit", systemImage: "circle.hexagongrid"){
+            Tab("Hábitos", systemImage: "circle.hexagongrid"){
                 NavigationStack{
                     HabitView(viewModel: HabitsViewModel(habitCompletionService: HabitCompletionRepository(context: context), habitService: HabitRepository(context: context)))
                 }
             }
-            Tab("Stats", systemImage: "checkmark.arrow.trianglehead.counterclockwise"){
+            Tab("Resumo", systemImage: "checkmark.arrow.trianglehead.counterclockwise"){
                 NavigationStack{
                     StatsView()
                 }
             }
-            Tab("Settings", systemImage: "gearshape"){
+            Tab("Configurações", systemImage: "gearshape"){
                 NavigationStack{
                     ConfigView()
                 }
             }
-            // MARK:- Tabfake
-//            Tab("Notif Timer", systemImage: "circle"){
-//                NavigationStack{
-//                    notificationTest()
-//                }
-//            }
-//            Tab("Notif Agendada", systemImage: "circle"){
-//                NavigationStack{
-//                    Notification2Tests()
-//                }
-//            }
-//            Tab("viewModelTests", systemImage: "circle.fill") {
-//                NavigationStack {
-//                    HabitsPOCView(viewModel: .init(habitCompletionService: HabitCompletionRepository(context: context),
-//                                                             habitService: HabitRepository(context: context))
-//                    )
-//                }
-//            }
         }
         .tint(.indigoCustom)
     }
